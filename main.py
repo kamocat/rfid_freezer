64098ff4c4ea4e70ea52cf7af7e54aa6845c01f9
Marshal <kamocat@gmail.com>
2023-02-21 23:56:59 -0800

    Switched form to post

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from typing import Union
+from datetime import date
+
+class Food(BaseModel):
+    name: str
+    qty: int
+    lbs: Union[float,None] = None
+    fr1: bool
+    fr2: bool
+    fr3: bool
+    tag: int
+    notes: Union[str,None] = None
+    freeze: date
+    thaw: Union[date,None] = None
+
+
+
 
 app = FastAPI()
 
@@ -11,9 +29,19 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 def today():
-    from datetime import date
     return date.today().strftime("%Y-%m-%d")
 
 @app.get("/add", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse("add.html", {"request": request, "today": today()})
+async def get_add(request: Request):
+    return templates.TemplateResponse("add.html", 
+        {"request": request, 
+         "qty": 1,
+         "notes": "Add notes here...",
+         "freeze": today(),
+        })
+
+@app.post("/add")
+async def post_add(item: Food):
+    return item
+    return templates.TemplateResponse("add.html", item)
+
